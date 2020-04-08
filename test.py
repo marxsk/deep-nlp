a15e6706623589dd504134086664af1e74cefa34
Marek Grac <marek@bedeep.eu>
2020-04-08 19:12:11 +0200

    [fix] Fix failing tests after adding epsilon terminal

--- test.py
+++ test.py
@@ -2,22 +2,29 @@ import unittest
 
 from pipeline import preprocessor
 
+# @todo: create own assertEqual that will call preprocessor and adds self.permanent_suffix
+
 
 class TestPreprocessor(unittest.TestCase):
+    permanent_suffix = '\nempty:\n%ignore " "'
+
     def test_comments_are_removed(self):
         grammar = "// this is comment line"
-        self.assertEqual(preprocessor(grammar), "")
+        self.assertEqual("\n" + preprocessor(grammar),
+                         "" + self.permanent_suffix)
 
     def test_commands_are_left_untouched(self):
         grammar = "\%ignore abc"
-        self.assertEqual(preprocessor(grammar), '\%ignore abc')
+        self.assertEqual(preprocessor(grammar),
+                         '\%ignore abc' + self.permanent_suffix)
 
     def test_empty_lines_are_removed(self):
         grammar = """
 
 
         """
-        self.assertEqual(preprocessor(grammar), "")
+        self.assertEqual("\n" + preprocessor(grammar),
+                         "" + self.permanent_suffix)
 
     def test_order_of_rules_is_untouched(self):
         """ Test if the content of rule is untouched """
@@ -26,12 +33,13 @@ class TestPreprocessor(unittest.TestCase):
         foo: bar
         """
         self.assertEqual(preprocessor(grammar).strip(),
-                         'sentence:(foo)\nfoo:(bar)')
+                         'sentence:(foo)\nfoo:(bar)' + self.permanent_suffix)
 
     def test_parentheses_are_added_to_right_side(self):
         """ Test if the parentheses are added to the right side of the rule """
         grammar = "   sentence: foo  "
-        self.assertEqual(preprocessor(grammar).strip(), 'sentence:(foo)')
+        self.assertEqual(preprocessor(grammar).strip(),
+                         'sentence:(foo)' + self.permanent_suffix)
 
     def test_merge_of_two_left_sides(self):
         """ Test if the left sides are merged if they are across multiple lines """
@@ -39,7 +47,8 @@ class TestPreprocessor(unittest.TestCase):
             sentence: foo
             sentence: bar
         """
-        self.assertEqual(preprocessor(grammar).strip(), 'sentence:(foo)|(bar)')
+        self.assertEqual(preprocessor(grammar).strip(),
+                         'sentence:(foo)|(bar)' + self.permanent_suffix)
 
     def test_merge_of_two_left_sides_with_inserted_lines(self):
         grammar = """
@@ -49,21 +58,21 @@ class TestPreprocessor(unittest.TestCase):
             sentence: bar
         """
         self.assertEqual(preprocessor(grammar).strip(),
-                         'sentence:(foo)|(bar)\nfoo:(bar)')
+                         'sentence:(foo)|(bar)\nfoo:(bar)' + self.permanent_suffix)
 
     def test_if_epsilon_nonterminal_was_added_for_terminal(self):
         grammar1 = """
             TERMINAL: "foo"
         """
         self.assertEqual(preprocessor(grammar1).strip(),
-                         'TERMINAL:("foo")\neps_terminal: TERMINAL | empty')
+                         'TERMINAL:("foo")\neps_terminal: TERMINAL | empty' + self.permanent_suffix)
 
         grammar2 = """
             TERMINAL: "foo"
             TERMINAL: "bar"
         """
         self.assertEqual(preprocessor(grammar2).strip(),
-                         'TERMINAL:("foo")|("bar")\neps_terminal: TERMINAL | empty')
+                         'TERMINAL:("foo")|("bar")\neps_terminal: TERMINAL | empty' + self.permanent_suffix)
 
 
 if __name__ == '__main__':
